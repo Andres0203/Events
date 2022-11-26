f6b2ebf47f9ea1a6f20fa1fedcaf64dc042b8908
Andres0203 <fandressanchezm@gmail.com>
2022-11-25 23:29:47 -0500

    Splash_Uno

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,18 @@
+import 'package:events/src/pages/splashpage.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    ThemeData.dark();
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: SplashScreen(),
     );
   }
-}
-
-@override
-Widget build(BuildContext context) {
-  return Scaffold();
 }
---
+++ lib/src/pages/splashpage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: FadeOut(
+        delay: Duration(milliseconds: 600),
+        child: ZoomIn(
+          // delay: Duration(seconds: 2),
+          from: 10,
+          duration: Duration(seconds: 1),
+          child: Image.asset(
+            "assets/images/events.png",
+            scale: 20,
+          ),
+        ),
+      )),
+    );
+  }
+}
